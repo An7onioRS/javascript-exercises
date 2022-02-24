9fd919a529f665bf8bcec7591801483eea294d61
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-02-24 20:53:45 +0200

    Exercise 2 has been completed!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitionNum) {
+    if (repetitionNum < 0) {
+        return 'ERROR';
+    }
+    let newString = '';
+    for (i = 0; i < repetitionNum; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
