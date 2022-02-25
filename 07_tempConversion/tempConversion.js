c334e36a408f0cc9894b7b26cd492cce2993f386
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-02-25 15:30:13 +0200

    Exercise 7 has been completed!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const rounded = floatingNumber => parseFloat((Math.round(floatingNumber * 10) / 10).toFixed(1));
 
+const ftoc = function(farenheit) {
+  let celsius = (farenheit - 32) * 5/9;
+  return rounded(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let farenheit = celsius * 9/5 + 32;
+  return rounded(farenheit);
 };
 
 // Do not edit below this line
