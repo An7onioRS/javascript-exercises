06b60cedfef9d11375a495032c13fbcc076bb77e
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-08 15:33:29 +0200

    Completed Exercise 8 - Calculator
    In this exercise I trained my ability to use array.reduce() method
    as well as created two different functions for finding a factorial of a
    number - 1) using iterations from num - 1 to 1 inclusively and 2) using
    recursion

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,21 @@
-const add = function() {
-	
+const add = (num1, num2) => num1 += num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const sum = (arr) => arr.reduce((acc, currItem) => acc += currItem, 0);
+
+const multiply = (arr) => arr.reduce((totalMultiplied, currItem) => totalMultiplied *= currItem, 1);
+
+const power = (num, power) => num ** power;
+
+const factorial = num => {
+  
+  if (num < 0) return -1;
+  if (num === 0) return 1;
+
+	return num * factorial(num -1); //  5 * factorial (4) = 4 * factorial(3)  = 3 * factorial(2) = 2 * factorial(1) = 1 * factorial(0) = returns 1 => factorial(1) = 1 * 1 = 1 => factorial(2) = 2 * 1 = 2 => factorial(3) = 3 * 2 = 6 => factorial(4) = 4 * 6 = 24 => factorial(5) = 5 * 24 = 120
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
